f7eebdbae79eb10d3a9381564e09252af9d2a3ae
karth <karthavyakumar16111998@gmail.com>
2025-02-10 15:26:12 +0530

    todevelop

--- src/main/java/org/example/demo1/Demo1hyy.java
+++ src/main/java/org/example/demo1/Demo1hyy.java
@@ -3,5 +3,6 @@ package org.example.demo1;
 public class Demo1hyy {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("from master to develop");
     }
 }
